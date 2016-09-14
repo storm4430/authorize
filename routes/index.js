340412f398edb478e42e4a7c4e931895aee6c971
storm4430 <storm4430@yandex.ru>
2016-09-14 16:42:05 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render("index", { title: "Система электронного документооборота" });
+});
+
+module.exports = router;
